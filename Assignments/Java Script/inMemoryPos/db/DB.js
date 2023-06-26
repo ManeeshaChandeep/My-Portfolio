bfaceaff2daa92de8e531c01c6b2e869a6bc8ef2
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-06-26 21:30:57 +0530

    transition

--- Assignments/Java Script/inMemoryPos/db/DB.js
+++ Assignments/Java Script/inMemoryPos/db/DB.js
@@ -1,13 +1,13 @@
 let customerDB = [
-    {id: "C00-001", name: "Isuru", address: "Galle", mobile: "0763880467", email: "isuru@gmail.com"},
-    {id: "C00-002", name: "Sachinda", address: "Panadura", mobile: "0770580467", email: "sachinda@gmail.com"},
-    {id: "C00-003", name: "Lakshan", address: "Galle", mobile: "0725880467", email: "lakshan@gmail.com"}
+    {id: "C00-001", name: "Kamal", address: "Galle", mobile: "07758425515", email: "kamal@gmail.com"},
+    {id: "C00-002", name: "Nimal", address: "Matara", mobile: "0758954525", email: "nimal@gmail.com"},
+    {id: "C00-003", name: "Pasindu", address: "Panadura", mobile: "07715458524", email: "pasindu@gmail.com"}
 ];
 
 
 let itemDB = [
-    {code: "I00-001", name: "Samsung S7", price: 50000, qty: 10},
-    {code: "I00-002", name: "Vivo Y19", price: 25000, qty: 20},
-    {code: "I00-003", name: "Samsung J7", price: 35000, qty: 5}
+    {code: "I00-001", name: "Samsung J7", price: 50000, qty: 10},
+    {code: "I00-002", name: "Samsung S7", price: 25000, qty: 20},
+    {code: "I00-003", name: "Vivo Y19", price: 35000, qty: 5}
 ];
 
